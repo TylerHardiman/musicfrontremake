2549623205c7f1a1fd09ef60b6d1a6e101c576c7
Tyler Hardiman <96437250+TylerHardiman@users.noreply.github.com>
2022-03-11 10:12:55 -0500

    chore: Created dropdown NavBar with built in search. Lots of css styling.

--- music-library-front-remake/pages/index.js
+++ music-library-front-remake/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
@@ -6,8 +7,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Music Library</a>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#link">Music Library</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -15,19 +16,34 @@ export default function Home() {
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#link">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#link">Music Table</a>
       </li>
-      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#link" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Music Resources
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Spotify link?</a>
+          <a class="dropdown-item" href="#">Music News link?</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Youtube link?</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search Library</button>
+    </form>
   </div>
 </nav>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Music Library
+         Music Library 
         </h1>
 
         <p className={styles.description}>
@@ -65,19 +81,17 @@ export default function Home() {
           </a>
         </div>
       </main>
+<footer>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="https://www.musicianshalloffame.com/">Musicians Hall of Fame and Museum Official Site</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse"  target="_blank" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="True" aria-label="Toggle navigation" rel="noopener noreferrer">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+      </nav>
+      </footer> 
+        </div>
+        
+  );
+       
 
-      <footer className={styles.footer}>
-        <a
-          href="https://www.billboard.com/charts/hot-100/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-          <span className={styles.logo}>
-            <p>Current Billboard Top 100</p>
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
-}
+  }
